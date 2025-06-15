da4aaba530dfcd8f513abd359c75fecd49e08023
YEOP <64470512+geonyeop123@users.noreply.github.com>
2025-06-15 15:46:47 +0900

    init

---
+++ src/main/java/com/premiumtrade/PremiumTradeApplication.java
@@ -0,0 +1,13 @@
+package com.premiumtrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremiumTradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PremiumTradeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/premiumtrade/PremiumTradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.premiumtrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremiumTradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
